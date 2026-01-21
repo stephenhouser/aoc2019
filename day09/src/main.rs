bed7962f62adb836c43ab46c9ad19c7541b2de40
Stephen Houser <stephenhouser@gmail.com>
2026-01-21 15:49:05 -0500

    day 09 part 1

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -15,22 +15,37 @@ struct CPU {
 }
 
 impl CPU {
-    fn store(&mut self, address: usize, mode: i64, value: i64) {
-        if self.memory.len() <= address {
-            // println!("resize to {address}");
-            self.memory.resize(address+1, 0);
-        }
-
-        // println!("save {address} = {value}");
-        self.memory[address] = value;
-    }
-
     fn load_direct(&self, address: usize) -> i64 {
         if self.memory.len() <= address {
             return 0;
         }
 
         return self.memory[address];
+    }
+
+    fn store(&mut self, address: usize, mode: i64, value: i64) {
+        let operand = self.load_direct(address);
+
+        let mut position_mode = |operand, value| {
+            let address = usize::try_from(operand).expect("Bad memory address");
+            if self.memory.len() <= address {
+                self.memory.resize(address+1, 0);
+            }
+
+            println!("WRITE: {address}: {value}");
+            self.memory[address] = value;
+        };
+
+        let mut relative_mode = |operand, value| {
+            position_mode(self.base + operand, value);
+        };
+
+        match mode {
+            0 => { position_mode(operand, value) },
+            1 => {  },
+            2 => { relative_mode(operand, value) },
+            _ => {  }
+        }
     }
 
     fn load(&self, address: usize, mode: i64) -> i64 {
@@ -58,16 +73,16 @@ impl CPU {
         assert!(mode.len() >= 2);
         let o1 = self.load(self.pc + 1, mode[0]);
         let o2 = self.load(self.pc + 2, mode[1]);
-        let o3 = self.load(self.pc + 3, 1); // writes always use direct
+        // let o3 = self.load(self.pc + 3, 1); // writes always use direct
 
         if self.debug {
             let p1 = self.format_operand(self.pc + 1, mode[0]);
             let p2 = self.format_operand(self.pc + 2, mode[1]);
-            let p3 = self.format_operand(self.pc + 3, 1);
-            println!("add\t{p1}, {p2} -> {p3}");
+            let p3 = self.format_operand(self.pc + 3, mode[2]);
+            println!("add\t{p1}, {p2}, {p3}");
         }
 
-        self.store(usize::try_from(o3).unwrap(), o1 + o2);
+        self.store(self.pc + 3, mode[2], o1 + o2);
         self.pc += 4;
 
         return true;
@@ -77,16 +92,17 @@ impl CPU {
         assert!(mode.len() >= 2);
         let o1 = self.load(self.pc + 1, mode[0]);
         let o2 = self.load(self.pc + 2, mode[1]);
-        let o3 = self.load(self.pc + 3, 1); // writes always use direct
+        // let o3 = self.load(self.pc + 3, 1); // writes always use direct
 
         if self.debug {
             let p1 = self.format_operand(self.pc + 1, mode[0]);
             let p2 = self.format_operand(self.pc + 2, mode[1]);
-            let p3 = self.format_operand(self.pc + 3, 1);
-            println!("mul\t{p1}, {p2} -> {p3}");
+            let p3 = self.format_operand(self.pc + 3, mode[2]);
+            println!("mul\t{p1}, {p2}, {p3}");
         }
 
-        self.store(usize::try_from(o3).unwrap(), o1 * o2);
+        // self.store(usize::try_from(o3).unwrap(), o1 * o2);
+        self.store(self.pc + 3, mode[2], o1 * o2);
         self.pc += 4;
 
         return true;
@@ -94,25 +110,26 @@ impl CPU {
 
     fn inp(&mut self, mode: &Vec<i64>) -> bool {
         // assert!(mode.len() >= 0);
-        let o1 = self.load(self.pc + 1, mode[0]); // writes always use direct
+        // let o1 = self.load(self.pc + 1, mode[0]); // writes always use direct
 
         if self.input.len() >= 1 {
             let value = self.input.pop_front().unwrap();
 
             if self.debug {
-                let p1 = self.format_operand(self.pc + 1, 1);
-                println!("inp\t{p1} << {value}");
+                let p1 = self.format_operand(self.pc + 1, mode[0]);
+                println!("inp\t{p1} <== {value}");
             }
             
-            let address = usize::try_from(o1).unwrap();
-            self.store(address, value);
+            // let address = usize::try_from(o1).unwrap();
+            // self.store(address, value);
+            self.store(self.pc + 1, mode[0], value);
             self.pc += 2;
 
             return true;
         }
 
         if self.debug {
-            let p1 = self.format_operand(self.pc + 1, 1);
+            let p1 = self.format_operand(self.pc + 1, mode[0]);
             println!("inp\t{p1} << No input available (pause)\n");
         }
         
@@ -124,8 +141,8 @@ impl CPU {
         let o1 = self.load(self.pc + 1, mode[0]);
 
         if self.debug {
-            let p1 = self.format_operand(self.pc + 1, 1);
-            println!("out\t{p1} >> {o1}");
+            let p1 = self.format_operand(self.pc + 1, mode[0]);
+            println!("out\t{p1} ==> {o1}");
         }
 
         self.output.push_back(o1);
@@ -178,19 +195,21 @@ impl CPU {
         assert!(mode.len() >= 2);
         let o1 = self.load(self.pc + 1, mode[0]);
         let o2 = self.load(self.pc + 2, mode[1]);
-        let o3 = self.load(self.pc + 3, 1); // writes always use direct
+        // let o3 = self.load(self.pc + 3, 1); // writes always use direct
 
         if self.debug {
             let p1 = self.format_operand(self.pc + 1, mode[0]);
             let p2 = self.format_operand(self.pc + 2, mode[1]);
-            let p3 = self.format_operand(self.pc + 3, 1);
-            println!("lt\t{p1}, {p2} -> {p3}");
+            let p3 = self.format_operand(self.pc + 3, mode[2]);
+            println!("lt\t{p1}, {p2}, {p3}");
         }
 
         if o1 < o2 {
-            self.store(usize::try_from(o3).unwrap(), 1);
+            // self.store(usize::try_from(o3).unwrap(), 1);
+            self.store(self.pc + 3, mode[2], 1);
         } else {
-            self.store(usize::try_from(o3).unwrap(), 0);
+            // self.store(usize::try_from(o3).unwrap(), 0);
+            self.store(self.pc + 3, mode[2], 0);
         }
 
         self.pc += 4;
@@ -202,19 +221,21 @@ impl CPU {
         assert!(mode.len() >= 2);
         let o1 = self.load(self.pc + 1, mode[0]);
         let o2 = self.load(self.pc + 2, mode[1]);
-        let o3 = self.load(self.pc + 3, 1); // writes always use direct
+        // let o3 = self.load(self.pc + 3, 1); // writes always use direct
 
         if self.debug {
             let p1 = self.format_operand(self.pc + 1, mode[0]);
             let p2 = self.format_operand(self.pc + 2, mode[1]);
-            let p3 = self.format_operand(self.pc + 3, 1);
-            println!("eq\t{p1}, {p2} -> {p3}");
+            let p3 = self.format_operand(self.pc + 3, mode[2]);
+            println!("eq\t{p1}, {p2}, {p3}");
         }
 
         if o1 == o2 {
-            self.store(usize::try_from(o3).unwrap(), 1);
+            // self.store(usize::try_from(o3).unwrap(), 1);
+            self.store(self.pc + 3, mode[2], 1);
         } else {
-            self.store(usize::try_from(o3).unwrap(), 0);
+            // self.store(usize::try_from(o3).unwrap(), 0);
+            self.store(self.pc + 3, mode[2], 0);
         }
 
         self.pc += 4;
@@ -317,23 +338,20 @@ impl CPU {
         let operand = self.load_direct(address);
 
         let format_position = |operand| {
-            let addr = usize::try_from(operand).unwrap();
-            // let value = self.memory[addr];
-            let value = self.load_direct(addr);
-            format!("*{address}={addr} ({value})")
+            format!("{operand}")
         };
 
-        let format_direct = |operand| {
-            format!("{address} ({operand})")
+        let format_immediate = |operand| {
+            format!("${operand}")
         };
 
         let format_relative = |operand| {
-            format_position(self.base + operand)
+            format!("+{operand}")
         };
 
         match mode {
             0 => { format_position(operand) },
-            1 => { format_direct(operand) },
+            1 => { format_immediate(operand) },
             2 => { format_relative(operand) },
             _ => { format!("Error") }
         }
@@ -365,7 +383,7 @@ fn build_cpu(program: &Vec<i64>, input: &Vec<i64>) -> CPU {
         base: 0,
         input: VecDeque::from_iter(input.clone()),
         output: VecDeque::new(),
-        debug: false,
+        debug: true,
     };
 }
 
