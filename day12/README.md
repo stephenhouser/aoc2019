c10e2dd62a1b0df0a7ff5dc1cf4aec7ab2339426
Stephen Houser <stephenhouser@gmail.com>
2026-01-26 10:35:29 -0500

    day 12 part 2 working on it

--- day12/README.md
+++ day12/README.md
@@ -1,8 +1,27 @@
 # Day 12: The N-Body Problem
 
-Points and Vectors
+Points, Vectors, Iteration
 
 ## Part 1
 
+simulate movement of moons based on gravity changing the velocity and
+the velocity changing positions.
+
+Mostly detailed work to get the sequence correct.
 
 ## Part 2
+
+Find where the system begins to loop. Where matches a previous state.
+
+Flibbergibbets, I think it's a chinese remainder therom problem or similar.
+
+- for a moon; will repeat when all moon positions are same and my vel repeats
+    next velocity is based only on relative positions.
+
+- find a moon's cycle
+
+- for ecah moon cache positions+my_vel
+
+- simulate until each moon has repeated
+
+- multiply those together for least common 
--- day12/src/main.rs
+++ day12/src/main.rs
@@ -4,6 +4,10 @@ use std::fs;
 use std::cmp::Ordering;
 use std::time::Instant; // 0.8.2
 
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
 struct Point {
 	x: i64,
@@ -18,14 +22,42 @@ struct Body {
 }
 
 impl Point {
-	fn add(&mut self, other: &Point) {
-		self.x += other.x;
-		self.y += other.y;
-        self.z += other.z;
-	}
+	// fn add(&mut self, other: &Point) {
+	// 	self.x += other.x;
+	// 	self.y += other.y;
+    //     self.z += other.z;
+	// }
+
+    // fn equal(&self, other: &Point) -> bool {
+    //     return self.x == other.x && self.y == other.y && self.z == other.z;
+    // }
 } 
 
+// fn pair_gravity(b1: &Body, b2: &Body) -> Point {
+//     return Point {
+//         x: match b1.p.x.cmp(&b2.p.x) {
+//             Ordering::Greater => { -1 },
+//             Ordering::Equal => { 0 },
+//             Ordering::Less => { 1 },
+//         },
+//         y: match b1.p.y.cmp(&b2.p.y) {
+//             Ordering::Greater => { -1 },
+//             Ordering::Equal => { 0 },
+//             Ordering::Less => { 1 },
+//         },
+//         z: match b1.p.z.cmp(&b2.p.z) {
+//             Ordering::Greater => { -1 },
+//             Ordering::Equal => { 0 },
+//             Ordering::Less => { 1 },
+//         },
+//     }
+// }
+
 impl Body {
+    // fn equal(&self, other: &Body) -> bool {
+    //     return self.p.equal(&other.p) && self.velocity.equal(&other.velocity);
+    // }
+
     fn kenetic(&self) -> u64 {
         return (self.p.x.abs() + self.p.y.abs() + self.p.z.abs()) as u64;
     }
@@ -35,60 +67,59 @@ impl Body {
     }
 
     fn energy(&self) -> u64 {
-        // let k = self.kenetic();
-        // let p = self.potential();
-        // println!("{self:?} k={k},p={p}");
         return self.kenetic() * self.potential();
     }
-}
 
-fn pair_gravity(b1: &Body, b2: &Body) -> Point {
-    return Point {
-        x: match b1.p.x.cmp(&b2.p.x) {
-            Ordering::Greater => { -1 },
-            Ordering::Equal => { 0 },
-            Ordering::Less => { 1 },
-        },
-        y: match b1.p.y.cmp(&b2.p.y) {
-            Ordering::Greater => { -1 },
-            Ordering::Equal => { 0 },
-            Ordering::Less => { 1 },
-        },
-        z: match b1.p.z.cmp(&b2.p.z) {
-            Ordering::Greater => { -1 },
-            Ordering::Equal => { 0 },
-            Ordering::Less => { 1 },
-        },
-    }
-}
-
-fn physics(moons: &Vec<Body>) -> Vec<Body> {
-    // gravity is starting velocity of moon
-    let mut gravity: Vec<Point> = moons.iter()
-        .map(|moon| moon.velocity)
-        .collect();
-
-    // update with all pair-wise gravity
-    for i in 0..moons.len() {
-        for j in i+1..moons.len() {
-            gravity[i].add(&pair_gravity(&moons[i], &moons[j]));
-            gravity[j].add(&pair_gravity(&moons[j], &moons[i]));
-        }
-    }
-
-    return moons.iter().enumerate()
-        // apply gravity to moons
-        .map(|(i, moon)| Body { p: moon.p, velocity: gravity[i] })
-        // apply (calculated) velocity to moons
-        .map(|moon| Body { 
-            p: Point { 
-                x:moon.p.x + moon.velocity.x,
-                y:moon.p.y + moon.velocity.y,
-                z:moon.p.z + moon.velocity.z,
+    fn apply_velocity(&self) -> Body {
+        return Body {
+            p: Point {
+                x:self.p.x + self.velocity.x,
+                y:self.p.y + self.velocity.y,
+                z:self.p.z + self.velocity.z,
             },
-            velocity: moon.velocity 
-        })
-        .collect();
+            velocity: self.velocity
+        };
+    }
+
+    fn calculate_gravity(&self, moons: &Vec<Body>) -> Point {
+        let mut gravity = Point { x: 0, y: 0, z: 0 };
+
+        for other in moons {
+            gravity.x += match self.p.x.cmp(&other.p.x) {
+                Ordering::Greater => { -1 },
+                Ordering::Equal => { 0 },
+                Ordering::Less => { 1 },
+            };
+
+            gravity.y += match self.p.y.cmp(&other.p.y) {
+                Ordering::Greater => { -1 },
+                Ordering::Equal => { 0 },
+                Ordering::Less => { 1 },
+            };
+
+            gravity.z += match self.p.z.cmp(&other.p.z) {
+                Ordering::Greater => { -1 },
+                Ordering::Equal => { 0 },
+                Ordering::Less => { 1 },
+            };
+        }
+
+
+        return gravity;
+    }
+
+    fn apply_gravity(&self, gravity: &Point) -> Body {
+        return Body { 
+            p: self.p, 
+            velocity: {
+                Point {
+                    x: self.velocity.x + gravity.x,
+                    y: self.velocity.y + gravity.y,
+                    z: self.velocity.z + gravity.z,
+                }
+            }
+        };
+    }
 }
 
 fn parse_moon(input: &str) -> Body {
@@ -112,10 +143,6 @@ fn read_data(filename: &str) -> Vec<Body> {
 }
 
 fn part1(moons: &Vec<Body>) -> u64 {
-    // for moon in moons {
-    //     println!("{moon:?}");
-    // }
-
     let steps = match moons[0].p.y {
         0 => 10,    // test.txt
         -10 => 100, // test-2.txt
@@ -124,17 +151,11 @@ fn part1(moons: &Vec<Body>) -> u64 {
 
     let mut next = moons.clone();
     for _step in 1..=steps {
-        next = physics(&next);
-
-        // let energy: u64 = next.iter()
-        //     .map(|moon| moon.energy())
-        //     .sum();
-
-        // println!("{step:4}: {energy}");
-        // println!();
-        // for moon in &next {
-        //     println!("{moon:?}");
-        // }    
+        next = next.iter()
+            .map(|moon| (moon, moon.calculate_gravity(&next)))
+            .map(|(moon, gravity)| moon.apply_gravity(&gravity))
+            .map(|moon| moon.apply_velocity())
+            .collect();
     }
 
     let energy = next.iter()
@@ -144,7 +165,109 @@ fn part1(moons: &Vec<Body>) -> u64 {
     return energy;
 }
 
+// a hash key for the current position of all moons and the
+// velocity of moon n
+fn key_for(moon: &Body, gravity: &Point) -> u64 {
+    let mut hasher = DefaultHasher::new();
+
+    moon.p.x.hash(&mut hasher);
+    moon.p.y.hash(&mut hasher);
+    moon.p.z.hash(&mut hasher);
+
+    moon.velocity.x.hash(&mut hasher);
+    moon.velocity.y.hash(&mut hasher);
+    moon.velocity.z.hash(&mut hasher);
+
+    gravity.x.hash(&mut hasher);
+    gravity.y.hash(&mut hasher);
+    gravity.z.hash(&mut hasher);
+    
+    return hasher.finish();
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b > 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    if a == 0 || b == 0 {
+        0
+    } else {
+        (a * b) / gcd(a, b)
+    }
+}
+// Function to compute LCM of a vector
+fn lcm_v(numbers: &Vec<u64>) -> u64 {
+    if numbers.is_empty() {
+        return 0;
+    }
+
+    let mut result = numbers[0];
+    for &next in numbers.iter().skip(1) {
+        result = lcm(result, next);
+    }
+    result
+}
+
 fn part2(moons: &Vec<Body>) -> u64 {
+    let mut moon_caches: Vec<HashMap<u64, u64>> = Vec::new();
+    for moon in moons {
+        moon_caches.push(HashMap::new());
+    }
+
+    let mut repeater = vec![0; moons.len()];
+    let mut repeats =  vec![0; moons.len()];
+
+    let mut next = moons.clone();
+    // for step in 1..=2773 {
+    let mut step = 0;
+    // while repeater.iter().any(|&n| n == 0) {
+    while repeats.iter().any(|&n| n < 2) {
+        let gravity: Vec<_> = next.iter()
+            .map(|moon| moon.calculate_gravity(&next))
+            .collect();
+
+        // snapshot of current state of each moon for caching
+        let states: Vec<_> = next.iter().enumerate()
+            .map(|(i, moon)| key_for(moon, &gravity[i]))
+            .collect();
+
+        // print!("{step:4}: ");
+        // let m = next[0];
+        // let s = states[0];
+        // println!("{s}: {m:?}");
+    
+        for n in 0..moons.len() {
+            // if repeater[n] == 0 {
+                if moon_caches[n].contains_key(&states[n]) {
+                    let h = states[n];
+                    let first = moon_caches[n].get(&states[n]).unwrap();
+                    let diff = step - first;
+                    println!("Moon {n} repeats at step {step:6} from {first:6} - {diff:6}");
+                    moon_caches[n].insert(states[n], step);
+                    repeater[n] = step;
+                    repeats[n] += 1;
+                } else {
+                    moon_caches[n].insert(states[n].clone(), step.clone());
+                }    
+            // }
+        }
+
+        step += 1;
+        next = next.iter()
+            .map(|moon| (moon, moon.calculate_gravity(&next)))
+            .map(|(moon, gravity)| moon.apply_gravity(&gravity))
+            .map(|moon| moon.apply_velocity())
+            .collect();
+    }
+
+    let result = lcm_v(&repeater);
+    println!("repeater={repeater:?} => {result}");
 
     return moons.len() as u64;
 }
