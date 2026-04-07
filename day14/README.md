3acfacbc8a98c275f60c5af560bbb6c3f8e22726
Stephen Houser <stephenhouser@gmail.com>
2026-04-07 17:23:44 -0400

    day 14 part 1 and 2

--- day14/README.md
+++ day14/README.md
@@ -1,21 +1,30 @@
-# Day 13: Care Package
+# Day 14: Care Package
 
-Intcode variation
+A recursive problem perhaps, did it iteratively thinking part 2 would
+kill recursion.
 
 ## Part 1
 
-More or less, pump the Intcode computer...
+This one took a long time, lots of breaks in between, just
+did not have the energy and focus.
 
-Run the program, collect the outputs in chunks of 3, count
-the ones with type == 2 (blocks). Seems to be no overlap, 
-so don't need the screen.
+Parsing was done early, changed data representation a few times to
+suit changing view of problem.
+
+Wanted iterative solution.
+Rules for production kep in HashMap for easy access.
+Needed to keep track of surplus elements (chemicals) created,
+only realized this after getting initial solution working.
+
+Put what you want in a queue, then process the queue, working
+backwards through the rules, adding to the queue the things
+that were needed to produce the element (in quantity).
+
+When you only have ore to be produced, you are done.
 
 ## Part 2
 
-- set memory[0] = 2 (free play)
-- input is joystick -1, 0, 1
-- output (-1, 0) is display value is score
-
-Beat the game by breaking blocks
-
-(do I need visualization?)
+Use solution for part 1 and binary search for solution.
+use lower bound as 1 trillion / the cost to produce one FUEL.
+use upper bound as 1 trillion / (the cost to produce / 2)
+The upper bound seems overly generous.
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -3,156 +3,122 @@ use std::process;
 use std::fs;
 use std::time::Instant; // 0.8.2
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 #[derive(Clone, Debug)]
-struct Component {
-    n: i64,
-    element: String,
+struct Reaction {
+    input: Vec<(String, u128)>,
+    output: String,
+    output_count: u128,
 }
 
-#[derive(Clone, Debug)]
-struct Production {
-    output: Componenet,
-    elements: Vec<Component>,
-}
-
-fn read_component(text: &str) -> Component {
+fn read_component(text: &str) -> (String, u128) {
     let parts: Vec<&str> = text.split_whitespace().collect();
 
-    Component { 
-        n: parts[0].parse().unwrap(),
-        element: parts[1].to_string() }
+    (parts[1].to_string(), parts[0].parse().unwrap())
 }
 
-fn read_production(line: &str) -> Production {
+fn read_reaction(line: &str) -> Reaction {
     let parts: Vec<&str> = line.split("=>").collect();
 
-    let elements: Vec<Component> = parts[0].split(',')
+    let input = parts[0].split(',')
             .map(|text| read_component(text))
             .collect();
 
-    let output: read_component(parts[1]);
+    let output = read_component(parts[1]);
 
-    return Production {
-        output: output,
-        elements: elements,
+    return Reaction {
+        input: input,
+        output: output.0,
+        output_count: output.1
     };
 }
 
-fn read_data(filename: &str) -> HashMap<String, Production> {
+fn read_data(filename: &str) -> HashMap<String, Reaction> {
     // create vector of points and their value
     let rules = fs::read_to_string(filename)
         .expect("Expected input file to exist")
         .split('\n')
         .filter(|text| text.len() >= 1)
-        .map(|text| read_production(text))
-        .map(|rule| (rule.output.element, rule))
+        .map(|text| read_reaction(text))
+        .map(|rule| (rule.output.clone(), rule))
         .collect();
 
     return rules;
 }
 
-
-fn find_rule<'a>(rules: &'a Vec<Production>, element: &String) -> &'a Production {
-    for prod in rules {
-        if prod.out[0].element == *element {
-            return &prod;
-        }
-    }
-
-    return &rules[0];
-}
-
-
 // how many `base` to produce a `produce`
-fn satisfy(rules: &Vec<Production>) -> i64 {
-    let mut current: HashMap<String, i64> = HashMap::new();
-    let mut next: HashMap<String, i64> = HashMap::new();
-    let mut _extra: HashMap<String, i64> = HashMap::new();
+fn satisfy(rules: &HashMap<String, Reaction>, element: &String, count: u128, base: &String) -> u128 {
+    let mut surplus: HashMap<String, u128> = HashMap::new();
+    let mut queue: VecDeque<(String, u128)> = VecDeque::new();
+    queue.push_back((element.clone(), count));
 
-    println!("Start...");
-    current.insert("FUEL".to_string(), 1);
+    let mut base_count: u128 = 0;
+    while let Some((element, amount)) = queue.pop_front() {
+        //println!("Produce {} {}", amount, element);
+        let available = surplus.get(&element).copied().unwrap_or(0);
 
-    let mut changes = 1;
-    while changes != 0 {
-        changes = 0;
-
-        for (e, n) in current {
-
-            if e == "ORE" {
-                *next.entry(e).or_insert(0) += n;
-                continue;
-            }
-
-            print!("Making {}x{} needs ", n, e);
-
-            changes += 1;
-            let rule = find_rule(rules, &e);
-            let produced = rule.out[0].n; // => 10 A
-
-            for c in &rule.inp {            // 10 ORE
-                let mut needed_n = c.n; // produce c.n n-times
-                while needed_n > 0 {
-                    print!("{}x{}={}, ", c.n, c.element, needed_n);
-                    // enter that we needed to produce c.n of these
-                    *next.entry(c.element.clone()).or_insert(0) += n;
-                    needed_n -= produced;
-                }
-                print!("END{}={} ", c.element, needed_n);
-                if needed_n != 0 {
-                    print!("extra {}={} ", c.element, needed_n);
-                }
-            }
-
-            println!("");
-    for (e, n) in next.iter() {
-        println!("{}:{}", n, e);
-    }
-            println!("");
+        if amount <= available {
+            // we have enough surplus to satisfy this
+            surplus.insert(element, available - amount);
+            continue;
         }
 
-        current = next;
-        next = HashMap::new();
-        println!("");
+        let deficit = amount - available;
+        let reaction = rules.get(&element).expect("Element not found");
+
+        // Produce enough to satisfy deficit
+        // ceiling division
+        let batches = (deficit + reaction.output_count - 1) / reaction.output_count;
+
+        let produced = batches * reaction.output_count;
+        let new_surplus = produced - deficit;
+        surplus.insert(element, new_surplus);
+
+        // add new items to queue
+        for (input_element, input_count) in &reaction.input {
+            let total_needed = input_count * batches;
+            if input_element == base {
+                // handle ore, don't add to queue
+                base_count += total_needed;
+            } else {
+                queue.push_back((input_element.clone(), total_needed));
+            }
+        }
     }
 
-    for (e, n) in current.iter() {
-        println!("{}:{}", n, e);
-    }
-    // current => vec of current needed elements, 1 FUEL
-    // produced => vec of produced elementsA
-    //
-    // stock: HashMap<element, n>
-    // for e, n in stock
-    //   if e != ORE
-    //      need = find_out(e)
-    //      for e1, n1 in need
-    //          stock[e1] += n * n1
-    
-    return 0;
+    return base_count;
 }
 
-fn part1(rules: &Vec<Production>) -> usize {
-    for prod in rules {
-        for i in &prod.inp {
-            print!("{} {}, ", i.n, i.element);
-        }
-
-        print!("-> ");
-
-        for o in &prod.out {
-            print!("{} {}", o.n, o.element);
-        }
-        println!("");
-    }
-
-    satisfy(rules);
-
-    return rules.len();
+fn part1(rules: &HashMap<String, Reaction>) -> u128 {
+    satisfy(rules, &"FUEL".to_string(), 1, &"ORE".to_string())
 }
 
-fn part2(rules: &Vec<Production>) -> usize {
-    return rules.len();
+fn part2(rules: &HashMap<String, Reaction>) -> u128 {
+    let trillion: u128 = 1000000000000;
+    let ore_per_fuel = satisfy(rules, &"FUEL".to_string(), 1, &"ORE".to_string());
+
+    // track the highest amount of FUEL created (less than 1 trillion)
+    let mut best: u128 = 0;
+
+    // binary search for amount of FUEL created by 1000000000000 ORE
+    let mut lo: u128 = trillion / ore_per_fuel; 
+    let mut hi: u128 = trillion / (ore_per_fuel / 2);
+
+    while lo <= hi {
+        let fuel = (lo + hi) / 2;
+        let ore_used = satisfy(rules, &"FUEL".to_string(), fuel, &"ORE".to_string());
+        //println!("lo={}, fuel={}, hi={}, ore_used={}", lo, fuel, hi, ore_used);
+
+        if ore_used <= trillion {
+            lo = fuel + 1;
+            best = fuel;
+        } else {
+            hi = fuel - 1;
+        }
+    }
+
+    return best;
 }
 
 fn main() {
@@ -165,27 +131,15 @@ fn main() {
     let start = Instant::now();
 
     let data = read_data(&args[1]);
-    println!(
-        "          parse ({:9.9} ns)",
-        Instant::now().duration_since(start).as_nanos()
-    );
+    println!("          parse ({:9.9} ns)", Instant::now().duration_since(start).as_nanos());
 
     let p1_start = Instant::now();
     let p1 = part1(&data);
-    println!(
-        "{p1:>15} ({:9.9} ns)",
-        Instant::now().duration_since(p1_start).as_nanos()
-    );
+    println!("{p1:>15} ({:9.9} ns)", Instant::now().duration_since(p1_start).as_nanos());
 
     let p2_start = Instant::now();
     let p2 = part2(&data);
 
-    println!(
-        "{p2:>15} ({:9.9} ns)",
-        Instant::now().duration_since(p2_start).as_nanos()
-    );
-    println!(
-        "          total ({:9.9} ns)",
-        Instant::now().duration_since(start).as_nanos()
-    );
+    println!("{p2:>15} ({:9.9} ns)", Instant::now().duration_since(p2_start).as_nanos());
+    println!("          total ({:9.9} ns)", Instant::now().duration_since(start).as_nanos());
 }
